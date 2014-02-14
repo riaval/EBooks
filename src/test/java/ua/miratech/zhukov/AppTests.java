13db26e11c5bf37da9f2ad1541e67ff5682e9e34
unknown <ls2294@gmail.com>
2014-02-14 13:27:02 +0200

    mvn package

--- src/test/java/ua/miratech/zhukov/AppTests.java
+++ src/test/java/ua/miratech/zhukov/AppTests.java
@@ -17,23 +17,25 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/test-context/test-applicationContext.xml",
+})
 public class AppTests {
-//    private MockMvc mockMvc;
-//
-//    @SuppressWarnings("SpringJavaAutowiringInspection")
-//    @Autowired
-//    protected WebApplicationContext wac;
-//
-//    @Before
-//    public void setup() {
-//        this.mockMvc = webAppContextSetup(this.wac).build();
-//    }
-//
-//    @Test
-//    public void simple() throws Exception {
+    private MockMvc mockMvc;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    protected WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void simple() throws Exception {
 //        mockMvc.perform(get("/"))
-//                .andExpect(status().isOk())
+//                .andExpect(status().isNotFound())
 //                .andExpect(view().name("index"));
-//    }
+    }
 }
